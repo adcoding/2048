34fc72e92ef7b19831f591aebaffefd3aac83683
Adele <50012804+adcoding@users.noreply.github.com>
2021-11-11 09:17:30 +0100

    Add files via upload

--- 2048/Scripts/Piece.gd
+++ 2048/Scripts/Piece.gd
@@ -5,12 +5,17 @@ export var value: int
 export var next_value: PackedScene
 
 onready var move_tween := $MoveTween
+onready var size_tween := $SizeTween
+onready var modulate_tween := $ModulateTween
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func pop_in():
+	size_tween.interpolate_property(self, "scale", Vector2(0.1, 0.1), Vector2(1, 1), 0.2, Tween.TRANS_SINE, Tween.EASE_OUT )
+	size_tween.start()	 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -21,4 +26,21 @@ func move(new_position: Vector2):
 	move_tween.start()
 
 func remove():
-	queue_free()
+	size_tween.interpolate_property(self, "scale", scale, Vector2(1.5, 1.5), 0.2, Tween.TRANS_SINE, Tween.EASE_OUT)
+	modulate_tween.interpolate_property(self, "modulate", modulate, Color(0, 0, 0, 0), 0.2, Tween.TRANS_SINE, Tween.EASE_OUT)
+	size_tween.start()
+	modulate_tween.start()
+
+
+func _on_ModulateTween_tween_completed(object, key):
+	if scale == Vector2(1.5, 1.5):
+		queue_free()
+
+
+
+
+
+
+
+
+
